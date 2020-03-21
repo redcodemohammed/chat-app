a98c4da38a923ddb190d1a2a22936d5c07fa9cfc
redcodemohammed <redcodemohammed@gmail.com>
2020-03-21 20:25:55 +0300

    Removed unused buttons

--- src/components/Theme/AppBar.vue
+++ src/components/Theme/AppBar.vue
@@ -7,14 +7,6 @@
 
       <v-spacer></v-spacer>
 
-      <v-btn icon class="hidden-sm-and-down">
-        <v-icon>mdi-heart</v-icon>
-      </v-btn>
-
-      <v-btn icon class="hidden-sm-and-down">
-        <v-icon>mdi-magnify</v-icon>
-      </v-btn>
-
       <v-menu left bottom class="hidden-sm-and-down">
         <template v-slot:activator="{ on }">
           <v-btn icon v-on="on" class="hidden-sm-and-down">
